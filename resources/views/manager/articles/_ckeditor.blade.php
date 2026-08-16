25d7d85f5cd237ba80b26b723c4d00391655d64a
hoangtk <concrete@CC-Airs-MacBook-Air.local>
2026-08-16 12:48:47 +0700

    feat: add CKEditor 5 partial for article content editing

---
+++ resources/views/manager/articles/_ckeditor.blade.php
@@ -0,0 +1,61 @@
+{{-- CKEditor 5 partial. Pass $initialContent (string) to pre-populate. --}}
+<script src="https://cdn.jsdelivr.net/npm/ckeditor5@48.4.0/dist/browser/ckeditor5.umd.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/ckeditor5@48.4.0/dist/browser/ckeditor5.css">
+<script>
+    const {
+        ClassicEditor, Essentials, Paragraph, Heading,
+        Bold, Italic, Underline, Strikethrough,
+        FontColor, FontBackgroundColor, Alignment,
+        List, Indent, BlockQuote, CodeBlock, Link,
+        Image, ImageUpload, ImageResize, ImageStyle, ImageToolbar, SimpleUploadAdapter,
+        Table, TableToolbar, MediaEmbed, PasteFromOffice,
+    } = CKEDITOR;
+
+    const ckCsrfToken = document.querySelector('meta[name="csrf-token"]').content;
+
+    ClassicEditor
+        .create(document.getElementById('content-editor'), {
+            licenseKey: 'GPL',
+            plugins: [
+                Essentials, Paragraph, Heading,
+                Bold, Italic, Underline, Strikethrough,
+                FontColor, FontBackgroundColor, Alignment,
+                List, Indent, BlockQuote, CodeBlock, Link,
+                Image, ImageUpload, ImageResize, ImageStyle, ImageToolbar, SimpleUploadAdapter,
+                Table, TableToolbar, MediaEmbed, PasteFromOffice,
+            ],
+            toolbar: [
+                'heading', '|',
+                'bold', 'italic', 'underline', 'strikethrough', '|',
+                'fontColor', 'fontBackgroundColor', 'alignment', '|',
+                'bulletedList', 'numberedList', 'outdent', 'indent', '|',
+                'blockQuote', 'codeBlock', 'link', 'uploadImage', 'insertTable', 'mediaEmbed', '|',
+                'undo', 'redo',
+            ],
+            image: {
+                toolbar: [
+                    'imageStyle:inline', 'imageStyle:block', 'imageStyle:side', '|',
+                    'toggleImageCaption', 'imageTextAlternative', 'resizeImage',
+                ],
+            },
+            table: {
+                contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells'],
+            },
+            simpleUpload: {
+                uploadUrl: '{{ route('manager.articles.upload-image') }}',
+                headers: { 'X-CSRF-TOKEN': ckCsrfToken },
+            },
+        })
+        .then(editor => {
+            window.contentEditor = editor;
+
+            @if(!empty($initialContent))
+            editor.setData({!! Js::from($initialContent) !!});
+            @endif
+
+            document.getElementById('articleForm').addEventListener('submit', function () {
+                document.getElementById('content-input').value = editor.getData();
+            });
+        })
+        .catch(error => console.error('CKEditor init failed:', error));
+</script>
